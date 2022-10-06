02742ae206396992d14a950011ad811425d6158f
Peter Levinsky <pele@easj.dk>
2022-10-06 09:56:26 +0200

    Add project files.

---
+++ ClassDemoConsumeBilREST/Consumer.cs
@@ -0,0 +1,52 @@
+﻿using BilModelLib.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoConsumeBilREST
+{
+    public class Consumer
+    {
+        private static string URL = "http://localhost:5191/api/Biler/";
+
+        public async Task<List<Bil>> GetAllAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(URL);
+                if (resp.IsSuccessStatusCode)
+                {
+                    List<Bil> cList = await resp.Content.ReadFromJsonAsync<List<Bil>>();
+                    return cList;
+                }
+                return new List<Bil>(); // evt. en exception
+            }
+        }
+
+
+        public async Task<Bil> PutAsync(string stelnummer, Bil ændretBil)
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                JsonContent content = JsonContent.Create(ændretBil);
+
+                HttpResponseMessage resp = await client.PutAsync(URL + stelnummer, content);
+                if (resp.IsSuccessStatusCode)
+                {
+                    Bil rBil = await resp.Content.ReadFromJsonAsync<Bil>();
+                    return rBil;
+                }
+
+                return new Bil();
+
+            }
+        }
+
+
+
+    }
+}
---
+++ ClassDemoConsumeBilREST/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BilModelLib.model;
+using ClassDemoConsumeBilREST;
+
+Console.WriteLine("Hello, World!");
+Consumer consumer = new Consumer();
+
+List<Bil> biler = await consumer.GetAllAsync();
+
+foreach (Bil bil in biler)
+{
+    Console.WriteLine(bil);
+}
+
+
+
+
+Bil ændretBil = new Bil()
+{ Model = "Polo", StelNummer = "XC3.45627.456", Km = 23000, Aar = 2018, Maerke = "VW", Braendsel = "Diesel" };
+
+Bil returBil = await consumer.PutAsync("XC3.45627.456", ændretBil);
+
+Console.WriteLine(returBil);
